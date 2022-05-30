90343ab6b5ec9d3de0479dadbbe6ba04e9794195
Andre Parada <andreparad98@gmail.com>
2022-05-29 20:06:41 -0400

    Adjusting First Config of DElete MOdal

---
+++ src/app/@theme/modals/excluir-dialog/excluir-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-dialog',
+  templateUrl: './excluir-dialog.component.html',
+  styleUrls: ['./excluir-dialog.component.css']
+})
+export class ExcluirDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
