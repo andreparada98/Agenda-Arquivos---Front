24ab697438f640a09d2ddb6710f2fc5aaa39d973
Andre Parada <andreparada98@gmail.com>
2022-05-30 18:57:06 -0400

    Add searchById, Delete and Create

--- src/app/pages/usuario/shared/usuario.service.ts
+++ src/app/pages/usuario/shared/usuario.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { UserModel } from '../../../models/user.model';
+import { UsuarioModel } from '../../../models/usuario.model';
 import { AbstractService } from '../../../services/abstract.service';
 
 @Injectable({
@@ -15,11 +16,26 @@ export class UsuarioService extends AbstractService<UserModel>{
   private userSubject: BehaviorSubject<UserModel>;
 
   constructor(private http: HttpClient) { 
-    super(http, 'accounts')
+    super(http, 'accounts/')
   }
 
   getAllUsers(): Observable<UserModel[]> {
       console.log(this.baseUrlService)
     return this.http.get<UserModel[]>(this.baseUrlService)
+  }
+
+  getById(id: Number): Observable<UsuarioModel>{
+    console.log('entrou', id)
+    return this.http.get<UsuarioModel>(this.baseUrlService + id )
+  }
+
+  delete(id): Observable<UserModel>{
+    console.log(this.baseUrlService)
+    console.log(id)
+    return this.http.delete<UserModel>(this.baseUrlService + id)
+  }
+
+  create(entity: UsuarioModel): Observable<UsuarioModel>{
+    return this.http.post<UsuarioModel>(this.baseUrlService, entity)
   }
 }
